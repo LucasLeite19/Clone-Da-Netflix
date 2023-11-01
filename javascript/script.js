a527fb3d502dccaed0128cc5e1465800ff5d30c1
Lucas Leite <lucas-caldeira.6@outlook.com>
2023-11-01 20:56:29 -0300

    Primeira Parte
    aqui estou criando a parte de login do usuário.

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+/*
+ alert(
+    "Esta página foi desenvolvida por Lucas Leite com o objetivo de criar uma réplica do serviço Netflix como um exercício de HTML e CSS, além de servir como um portfólio acessível a todos que visitem seu perfil no GitHub."
+  );
+} 
+*/
+function about(el) {
+  document.getElementById(el).style.display = "block";
+  var botao = document.getElementById('meuBotao');
+  botao.style.display = 'none';
+}
+
+
